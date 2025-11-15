bea02af1ccc0bba5f5d03a4c7d4df068c89cf815
Odalis Rosillo <odalis.rosillo@unl.edu.ec>
2025-11-15 18:20:13 -0500

    cambios

--- Metdos/src/metodos/BubbleSort.java
+++ Metdos/src/metodos/BubbleSort.java
@@ -1,4 +1,84 @@
 package metodos;
 
 public class BubbleSort {
-}
+        public static void sort(int[] a) {
+            if (a == null || a.length <= 1) {
+                return;
+            }
+            int n = a.length;
+            boolean huboIntercambio;
+
+            for (int i = 0; i < n - 1; i++) {
+                huboIntercambio = false;
+
+                for (int j = 0; j < n - i - 1; j++) {
+
+                    if (a[j] > a[j + 1]) {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        huboIntercambio = true;
+                    }
+                }
+
+                if (!huboIntercambio) {
+                    break;
+                }
+            }
+        }
+
+        public static void sort(int[] a, boolean trace) {
+            if (!trace) {
+                sort(a);
+                return;
+            }
+
+            if (a == null || a.length <= 1) {
+                System.out.println("Array ya ordenado o vacío");
+                return;
+            }
+
+
+            System.out.println("bubble sort");
+            System.out.println("Array inicial: " + arrayToString(a));
+
+            int n = a.length;
+            boolean huboIntercambio;
+            int pasada = 0;
+
+            for (int i = 0; i < n - 1; i++) {
+                huboIntercambio = false;
+                pasada++;
+
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (a[j] > a[j + 1]) {
+                        int temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                        huboIntercambio = true;
+                    }
+                }
+
+                System.out.println("pasada " + pasada + ": " + arrayToString(a));
+                if (!huboIntercambio) {
+                    System.out.println("array ya ordenado");
+                    break;
+                }
+            }
+
+            System.out.println("array final: " + arrayToString(a));
+        }
+
+        private static String arrayToString(int[] a) {
+            StringBuilder sb = new StringBuilder("[");
+            for (int i = 0; i < a.length; i++) {
+                sb.append(a[i]);
+                if (i < a.length - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+
+    }
--- Metdos/src/metodos/InsertionSort.java
+++ Metdos/src/metodos/InsertionSort.java
@@ -1,4 +1,66 @@
 package metodos;
 
+import java.util.Arrays;
+
 public class InsertionSort {
-}
+        public static void sort(int[] a) {
+            if (a == null || a.length <= 1) {
+                return;
+            }
+            for (int i = 1; i < a.length; i++) {
+                int valorActual = a[i];
+                int posicion = i - 1;
+
+                while (posicion >= 0 && a[posicion] > valorActual) {
+                    a[posicion + 1] = a[posicion];
+                    posicion--;
+                }
+
+                a[posicion + 1] = valorActual;
+            }
+        }
+        public static void sort(int[] a, boolean trace) {
+            if (!trace) {
+                sort(a);
+                return;
+            }
+
+            if (a == null || a.length <= 1) {
+                System.out.println("Array ya ordenado o vacío");
+                return;
+            }
+
+            System.out.println("insertion sort");
+            System.out.println("Array inicial: " + arrayToString(a));
+
+            for (int i = 1; i < a.length; i++) {
+                int valorActual = a[i];
+                int posicion = i - 1;
+
+                System.out.println("iteración " + i + ": insertando " + valorActual);
+
+                while (posicion >= 0 && a[posicion] > valorActual) {
+                    a[posicion + 1] = a[posicion];
+                    posicion--;
+                }
+
+                a[posicion + 1] = valorActual;
+                System.out.println("luego de insertar: " + arrayToString(a));
+            }
+
+            System.out.println("Array final: " + arrayToString(a));
+        }
+
+        // para mostrar el arreglo de mejor forma
+        private static String arrayToString(int[] a) {
+            StringBuilder sb = new StringBuilder("[");
+            for (int i = 0; i < a.length; i++) {
+                sb.append(a[i]);
+                if (i < a.length - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("]");
+            return sb.toString();
+        }
+    }
